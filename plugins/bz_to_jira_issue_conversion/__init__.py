4bf4ce03860c3c9e77f5ad500fd657aff41cb24a
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2020-05-22 18:17:12 +0100

    back to python 2.7

--- plugins/bz_to_jira_issue_conversion/__init__.py
+++ plugins/bz_to_jira_issue_conversion/__init__.py
@@ -1,5 +1,6 @@
 # encoding=UTF-8
 """__init__.py"""
+from __future__ import print_function
 import re
 import csv
 
@@ -8,25 +9,23 @@ def build_filter(args):
 
 class Filter:
     def __init__(self, args):
-        if not isinstance(args, bytes):
-            args = args.encode('utf8')
         with open(args) as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             self.issue_map = { row[0].encode(): row[1].encode() for row in reader}
-        self.bz_replace_pattern = re.compile(r"(?i)(\(?(issue|bz|bug|bugzilla|bugzilla bug)[ /]*#?(\d+)\)?)".encode())
+        self.bz_replace_pattern = re.compile(r"(?i)(\(?(issue|bz|bug|bugzilla|bugzilla bug)[ /]*#?(\d+)\)?)")#.encode())
 
     def commit_message_filter(self, commit_data):
         for match in self.bz_replace_pattern.findall(commit_data['desc']):
             full_match = match[0]
-            if full_match in [b'bz2', b'BZ2', b'Bz2']:
+            if full_match in ['bz2', 'BZ2', 'Bz2']:
                 continue
             try:
                 bz_number = match[2]
             except IndexError:
-                raise ValueError(f'invalid match [{match}] in {commit_data["desc"]}')
+                raise ValueError('invalid match [{}] in {}'.format(match, commit_data['desc']))
             try:
                 jira_ticket = self.issue_map[bz_number]
                 commit_data['desc'] = commit_data['desc'].replace(
-                    full_match, f"(JIRA {jira_ticket.decode()}) (Was BZ {bz_number.decode()})".encode())
+                    full_match, "(JIRA {}) (Was BZ {})".format(jira_ticket, bz_number))
             except KeyError:
                 pass
